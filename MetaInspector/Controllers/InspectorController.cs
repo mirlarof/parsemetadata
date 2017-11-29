c0504e321fbc556bbf4cade66911db9f3992a927
Victor Balbo <victorb@take.net>
2017-11-29 17:40:14 -0200

    Add project

---
+++ MetaInspector/Controllers/InspectorController.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Cors;
+
+namespace MetaInspector.Controllers
+{
+    [Route("")]
+    public class InspectorController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [EnableCors("AllowAllOrigins")]
+        public MetaData Get([FromQuery]string url)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var content = client.GetAsync(url).Result.Content.ReadAsStringAsync())
+                {
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(content.Result);
+                    var metaTags = doc.DocumentNode.SelectNodes("//meta");
+                    var metaData = new MetaData();
+                    foreach (var tag in metaTags)
+                    {
+                        var tagName = tag.Attributes["name"]?.Value.ToLower();
+                        var tagContent = tag.Attributes["content"];
+                        var tagProperty = tag.Attributes["property"]?.Value.ToLower();
+                        if (tagName == "title" || tagName == "twitter:title" || tagProperty == "og:title")
+                        {
+                            metaData.Title = string.IsNullOrEmpty(metaData.Title) ? tagContent.Value : metaData.Title;
+                        }
+                        else if (tagName == "description" || tagName == "twitter:description" || tagProperty == "og:description")
+                        {
+                            metaData.Description = string.IsNullOrEmpty(metaData.Description) ? tagContent.Value : metaData.Description;
+                        }
+                        else if (tagName == "twitter:image" || tagProperty == "og:image")
+                        {
+                            metaData.Image = string.IsNullOrEmpty(metaData.Image) ? tagContent.Value : metaData.Image;
+                        }
+                    }
+                    return metaData;
+                }
+            }
+        }
+    }
+}
---
+++ MetaInspector/MetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetaInspector
+{
+    public class MetaData
+    {
+        public string Title;
+        public string Description;
+        public string Image;
+
+        public MetaData()
+        {
+            Title = Description = Image = string.Empty;
+        }
+    }
+}
